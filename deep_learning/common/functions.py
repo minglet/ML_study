c712622a2b05072b10bb961e91366d1cb01ac6d1
Your Name <minglet@gmail.com>
2024-01-06 17:07:23 +0900

    deep learning chap 1

--- deep_learning/common/functions.py
+++ deep_learning/common/functions.py
@@ -8,3 +8,31 @@ def identity_function(x):
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+def softmax(x):
+    if x.ndim == 2:
+        # overflow를 막기 위해 입력값 중
+        # 최대값을 빼준다. 
+        x = x - x.max(axis=1, keepdims=True)
+        x = np.exp(x)
+        x /= x.sum(axis=1, keepdims=True)
+    elif x.dim == 1:
+        x = x - np.max(x)
+        x = np.exp(x) / np.sum(np.exp(x))
+
+    return x
+
+def cross_entopy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1) # 예측한 것들 중 가장 높은 즉 확률이 가장 높은 값의 인덱스를 출력
+
+    batch_size = y.shape[0]
+    
+    cross_entropy = np.log(y[np.arange(batch_size), t] + 1e-7)
+    loss = -np.sum(cross_entropy) / batch_size
+
+    return loss
--- deep_learning/common/layers.py
+++ deep_learning/common/layers.py
@@ -1,4 +1,6 @@
 import numpy as np
+from functions import softmax, cross_entopy_error
+
 
 # Matmul
 class Matmul():
@@ -58,4 +60,32 @@ class Affine():
 
         self.grads[0][...] = dW
         self.grads[1][...] = db
+        return dx
+
+class SoftmaxWithLoos:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None # softmax의 출력
+        self.t = None # 정답 레이블
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        # 정답 레이블이 원핫 벡터일 경우 정답의 인덱스로 변환
+        # cross_entropy_error()에 이미 있어서 굳이 필요 없을 듯
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+        
+        loss = cross_entopy_error(self.y, self.t)
+        return loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0] # t의 맨 앞 요소는가 배치 사이즈
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx /= batch_size
+
         return dx
